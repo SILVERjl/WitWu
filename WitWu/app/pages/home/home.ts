71305782b16204a3627b3b9a6496132a23aafcd9
SILVERjl <jedi@geekli.st>
2016-06-22 20:36:07 -0400

    Adding todolist switch, todo input, and add button

--- WitWu/app/pages/home/home.ts
+++ WitWu/app/pages/home/home.ts
@@ -4,8 +4,9 @@ import {NavController} from 'ionic-angular';
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
+
 export class HomePage {
+  todo = "todos"; //the same as this.todo
   constructor(private navController: NavController) {
-  
   }
 }
